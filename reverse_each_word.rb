47ad4ee630ca6df1ebfc4b5d157d6063ec58fdf9
Becky Phan <beckyphan@live.com>
2019-05-24 15:59:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  reverse_string = ""
   array.collect do |word|
-    reverse_string << word.reverse
-    reverse_string << " "
+    word.reverse
   end
 end
